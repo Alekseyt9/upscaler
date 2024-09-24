40c9cda31ba0f3cf128f072cf41b0062c05bca36
alekseyt9 <alekseyt9@gmail.com>
2024-09-24 15:18:37 +0300

    fixes

--- internal/back/services/store/migration/000001_create_users_table.down.sql
+++ internal/back/services/store/migration/000001_create_users_table.down.sql
@@ -1,12 +1,7 @@
---авторизация пользователя
 DROP TABLE users;
 
---глобальная очередь задач
 DROP TABLE queue;
 
-
--- файлы пользователя
 DROP TABLE userfiles;
 
--- transaction outbox для отправки в очередь сообщений
 DROP TABLE outbox;
